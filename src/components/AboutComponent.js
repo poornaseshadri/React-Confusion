bfcfb4791aa4d8d9c7fb3a5556831b2e783a4ad8
poorna <seshadripoornasaiprakash007@gmail.com>
2020-08-01 18:23:47 +0530

    Assignment2 Modified

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,26 +2,32 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
+
+function RenderLeader({leader})
+{
+    return(
+        <Media tag="li">
+        <Media left middle>
+            <Media object src={leader.image} alt={leader.name} />
+        </Media>
+        <Media body className="ml-5">
+          <Media heading>{leader.name}</Media>
+          <p>{leader.designation}</p>
+          <p>{leader.description}</p>
+        </Media>
+      </Media>
+
+    );
+}
+
 function About(props) {
-
-    const RenderLeader = props.leaders.map((leader) => {
+    const leaders = props.leaders.map((leader) => {
         return (
-                <div key={leader.id} className="col-12 mt-5">
-                <Media tag="li">
-                  <Media left middle>
-                      <Media object src={leader.image} alt={leader.name} />
-                  </Media>
-                  <Media body className="ml-5">
-                    <Media heading>{leader.name}</Media>
-                    <p>{leader.designation}</p>
-                    <p>{leader.description}</p>
-                  </Media>
-                </Media>
-              </div>
-
+            <div key={leader.id} className="col-12 mt-5">
+            <RenderLeader leader={leader}/>
+          </div>
         );
     });
-
     return(
         <div className="container">
             <div className="row">
@@ -78,7 +84,7 @@ function About(props) {
                 </div>
                 <div className="col-12">
                     <Media list>
-                        {RenderLeader}
+                        {leaders}
                     </Media>
                 </div>
             </div>
