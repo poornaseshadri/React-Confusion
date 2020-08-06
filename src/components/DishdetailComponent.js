19d2e53a8a9d0327baecaf0405d6007a18ff5185
poorna <seshadripoornasaiprakash007@gmail.com>
2020-08-06 10:59:43 +0530

    Fetch From server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,6 +3,7 @@ import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbIte
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => val && (val.length >= len);
@@ -113,7 +114,7 @@ class CommentsForm extends Component
             return(
             
                 <Card>
-                    <CardImg  top src={dish.image} alt={dish.name} />
+                    <CardImg  top src={baseUrl + dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
